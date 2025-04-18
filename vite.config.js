8c07ae6c6ae7d9b56406aaaa158483e5a88324d1
manager225 <manager225@mail.ru>
2025-04-18 12:59:23 +0400

    fix config

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,8 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
-  publicPath: `/fashion-pick/`,
+  base: '/fashion-pick/',
   plugins: [vue()],
 })
